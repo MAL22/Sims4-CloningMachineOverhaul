8847f11c1b3789b1e8be143169339f13e9196409
Mike <36205942+MAL22@users.noreply.github.com>
2021-07-11 16:52:59 -0400

    Removed incorrect configuration file.

--- settings.py
+++
@@ -1,8 +0,0 @@
-import os
-
-creator_name = 'MAL22'
-mods_folder = os.path.expanduser(
-    os.path.join('D:', os.sep, 'mimil', 'Documents', 'Electronic Arts', 'The Sims 4', 'Mods')
-)
-
-game_folder = os.path.join('C:', os.sep, 'Program Files (x86)', 'Games', 'The Sims 4')
