cb134c87bbbe668fe91bd5044244d47f7c1f6375
MAL22 <mimilavoie95@msn.com>
2021-07-25 21:17:45 -0400

    Updated template settings file.

--- settings.py
+++ settings.py
@@ -1,10 +1,13 @@
 import os
+import subprocess
 
 creator_name = 'MAL22'
 mod_name = 'Cloning Machine Overhaul'
+project_name = "CloningMachineOverhaul"
 
 game_folder = os.path.join('C:', os.sep, 'Program Files (x86)', 'Games', 'The Sims 4')
 mods_folder = os.path.expanduser(os.path.join('~', 'Documents', 'Electronic Arts', 'The Sims 4', 'Mods'))
+s4s_executable = os.path.join('C:', os.sep, 'Program Files (x86)', 'Sims 4 Studio', 'S4Studio.exe')
 
 # Folder within the project where source code is stored
 src_folder = 'src'
@@ -18,9 +21,11 @@ assets_folder = 'assets'
 # Temporary folder used to contain source files while the project is being compiled and packaged
 temp_folder = 'temp'
 
+# folder within the game mods folder where dev files will be stored during development for testing
+dev_folder = '!!modding'
+
 # root folder of the project
 root_path = os.path.dirname(os.path.realpath('__file__'))
-print(root_path)
 
 # /////////////////////////////////////////////////////////////////////////////
 # full paths to the various folders configured above
@@ -28,4 +33,5 @@ src_path = os.path.join(root_path, src_folder)
 build_path = os.path.join(root_path, build_folder)
 assets_path = os.path.join(root_path, assets_folder)
 temp_path = os.path.join(root_path, temp_folder)
-dev_path = os.path.join(mods_folder, '!!modding', 'Scripts')
+dev_path = os.path.join(mods_folder, dev_folder)
+dev_scripts_path = os.path.join(dev_path, 'Scripts')
--- src/cloningmachineoverhaul/injections/objects.py
+++ src/cloningmachineoverhaul/injections/objects.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+import inspect
+import os
+
 import services
 from cloningmachineoverhaul.enums.tunings import TuningId
+from m22lib.exceptions.exception_watcher import error_watcher
 from m22lib.tunings.tuning_utils import get_tuning
 from m22lib.utils.files import M22LogFileManager
 from m22lib.utils.injector import injector
@@ -8,32 +13,54 @@ from sims.sim_info_types import Age
 from sims4.resources import Types, get_resource_key
 from sims4.tuning.instance_manager import InstanceManager
 from services import get_instance_manager
-from sims4.tuning.tunable import HasTunableSingletonFactory, AutoFactoryInit, TunableReference, TunableMapping, Tunable
+from sims4.tuning.instances import HashedTunedInstanceMetaclass
+from sims4.tuning.tunable import HasTunableSingletonFactory, AutoFactoryInit, TunableReference, TunableMapping, Tunable, HasTunableReference
+from objects.definition_manager import DefinitionManager
+
+
+_log = M22LogFileManager()
 
 
 class TunableCloningMachineInteractions(HasTunableSingletonFactory, AutoFactoryInit):
     FACTORY_TUNABLES = {
         'cloning_machine_interactions': TunableMapping(
             description='',
-            key_type=Tunable(description='', tunable_type=str),
+            key_type=Tunable(description='', tunable_type=str, default="None"),
             value_type=TunableReference(
                 description='',
                 manager=get_instance_manager(Types.INTERACTION)
             )
-        ),
-
+        )
     }
 
 
-class CloningInteractionsInjector(HasTunableReference, metaclass=HashedTunedInstanceMetaclass, manager=services.get_instance_manager(Types.SNIPPET)):
-    INSTANCE_TUNABLES = TunableCloningMachineInteractions.TunableFactory()
+class CloningInteractionsInjector(HasTunableReference, metaclass=HashedTunedInstanceMetaclass, manager=get_instance_manager(Types.SNIPPET)):
+    INSTANCE_TUNABLES = {
+        'cloning_machine_interactions': TunableMapping(
+            description='',
+            key_type=Tunable(description='', tunable_type=str, default="None"),
+            value_type=TunableReference(
+                description='',
+                manager=get_instance_manager(Types.INTERACTION)
+            )
+        )
+    }
 
     @classmethod
+    @error_watcher()
     def _tuning_loaded_callback(cls):
-        tuning = get_resource_key(TuningId.CLONING_MACHINE_OBJECT, Types.OBJECT)
-        dict = {}
-        if tuning and hasattr(tuning, '_super_affordances'):
-            tuning._super_affordances += dict.values()
+        global _log
+        try:
+            # key = get_resource_key(TuningId.CLONING_MACHINE_OBJECT, Types.OBJECT)
+            definition_manager = services.definition_manager()
+            sa_list = list(cls.cloning_machine_interactions.values())
+            tuning = super(DefinitionManager, definition_manager).get(TuningId.CLONING_MACHINE_OBJECT)
+
+            if tuning and hasattr(tuning, '_super_affordances'):
+                tuning._super_affordances += tuple(sa_list)
+                _log.write(f"injected {len(sa_list)} super affordance(s)...")
+        except Exception as e:
+            _log.write(e)
 
 
 '''@injector(InstanceManager, 'load_data_into_class_instances')
@@ -61,3 +88,4 @@ def cmo_load_data_into_class_instances(original, self, *args, **kwargs) -> None:
             for test in tuning.test_globals:
                 if type(test) is SimInfoTest:
                     test.ages = test.ages + [Age.TODDLER]'''
+
