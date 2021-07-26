6d62156ab81a0d549e4a04364482d096d7f9996d
MAL22 <mimilavoie95@msn.com>
2021-07-25 21:27:06 -0400

    Updated template settings.py

--- settings.py
+++ settings.py
@@ -1,13 +1,12 @@
 import os
-import subprocess
 
-creator_name = 'MAL22'
-mod_name = 'Cloning Machine Overhaul'
-project_name = "CloningMachineOverhaul"
+creator_name = 'my creator name'
+mod_name = 'my mod name'
+project_name = "myprojectname"
 
-game_folder = os.path.join('C:', os.sep, 'Program Files (x86)', 'Games', 'The Sims 4')
+game_folder = os.path.join('C:', os.sep, 'Program Files (x86)', 'Origin Games', 'The Sims 4')
 mods_folder = os.path.expanduser(os.path.join('~', 'Documents', 'Electronic Arts', 'The Sims 4', 'Mods'))
-s4s_executable = os.path.join('C:', os.sep, 'Program Files (x86)', 'Sims 4 Studio', 'S4Studio.exe')
+s4s_executable = os.path.join('D:', os.sep, 'Program Files (x86)', 'Sims 4 Studio', 'S4Studio.exe')
 
 # Folder within the project where source code is stored
 src_folder = 'src'
@@ -22,7 +21,7 @@ assets_folder = 'assets'
 temp_folder = 'temp'
 
 # folder within the game mods folder where dev files will be stored during development for testing
-dev_folder = '!!modding'
+dev_folder = 'modding'
 
 # root folder of the project
 root_path = os.path.dirname(os.path.realpath('__file__'))
